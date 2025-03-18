297191253fabf97d15c2e0c2c48c64f8bf9d3aa7
Siham <siham.elmali@gatech.edu>
2025-03-17 22:41:35 -0700

    updated reqs and profit files

--- profit_calculator.py
+++ profit_calculator.py
@@ -426,8 +426,3 @@ class ProfitCalculatorApp:
         except ValueError:
             raise ValueError(
                 f"Invalid number in {field_name.replace('_', ' ').title()}")
-
-
-def normalize_field_name(field_name):
-    """Normalize the field name to match the entry keys"""
-    return field_name.lower().replace(" ", "_").replace("($)", "").replace("(%)", "")
