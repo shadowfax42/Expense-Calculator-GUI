fb2b1cd6f28fac7e3f323619a80371896803901e
Siham <siham.elmali@gatech.edu>
2025-03-21 11:45:35 -0700

    Added icon for both app and tinkter when it load. stored icon path in a .env file

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+
 import tkinter as tk
+from dotenv import load_dotenv, find_dotenv
+import os
 from profit_calculator import ProfitCalculatorApp
+
+
+load_dotenv(find_dotenv())
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = ProfitCalculatorApp(root)
+    ICON_PATH = os.environ.get("ICON_PATH", "")
+    root.iconbitmap(ICON_PATH)
     root.mainloop()
